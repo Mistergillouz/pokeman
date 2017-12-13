76058a66c80cb4ae31bab3c63ada3e7226a93822
Sylvain Rabette <sylvain.rabette@sap.com>
2017-12-13 15:14:40 +0100

    clean version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,62 @@
 import React from 'react'
-import PokemonList from 'components/PokemonList'
+import PokedexHelper from 'data/PokedexHelper'
+
+import MainPage from 'components/MainPage'
+import ZoomPage from 'components/ZoomPage'
+import Constants from 'data/Constants'
+import SpeciesTooltip from 'components/SpeciesTooltip'
+
+const PAGES = { MainPage: 'MainPage', ZoomPage: ' ZoomPage' };
 
 class App extends React.Component {
    constructor() {
         super(...arguments)
 
-        this.state = {};
+        this.state = {
+            page: PAGES.MainPage,
+            tooltipTypeId: -1,
+            tooltip: {}
+        }
+    }
 
-        this.state.ids = [];
-        for (let i = 1; i < 20; i++) {
-            this.state.ids.push(i);
+    eventHandler(args) {
+
+        switch (args.eventType) {
+
+            case Constants.EVENT.ShowTooltip: 
+            
+                this.setState({ tooltipTypeId: args.id, tooltip: { x: args.event.clientX, y: args.event.clientY } });
+                break;
+        
+            case Constants.EVENT.HideTooltip:
+                this.setState({ tooltipTypeId: -1 });
+                break;
+            
+            case Constants.EVENT.PokemonSelected:
+
+                this.setState({
+                    page: PAGES.ZoomPage,
+                    id: args.id
+                });
+
+                break;
+
+            case Constants.EVENT.ZoomPageClosed:
+
+                this.setState({
+                    page: PAGES.MainPage,
+                });
+
+                break;
         }
     }
 
     render() { 
         return (
             <div className='app'>
-                <PokemonList pokemons={this.state.ids}/>
+                <MainPage visible={ this.state.page === PAGES.MainPage } eventHandler = { this.eventHandler.bind(this) }/>
+                <ZoomPage visible={ this.state.page === PAGES.ZoomPage } id={ this.state.id } eventHandler = { this.eventHandler.bind(this) }/>
+                <SpeciesTooltip ref="speciesTooltip" id={ this.state.tooltipTypeId } x={ this.state.tooltip.x } y={ this.state.tooltip.y } eventHandler={ (args) => this.eventHandler(args)}/>
             </div>
         )
     }
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import PokedexHelper from 'data/PokedexHelper'
+import Constants from 'data/Constants'
+import Species from 'components/Species'
+import Lazimage from 'components/Lazimage'
 
 class Pokemon extends React.Component {
    
@@ -7,63 +10,42 @@ class Pokemon extends React.Component {
         super(...arguments)
     }
 
+    buildSpecies(id) {
+        return (<Species key={id} id={id} eventHandler={ this.props.eventHandler }/>);
+    }
+
+    onPokemonClicked(event) {
+        if (this.props.eventHandler) {
+            this.props.eventHandler({
+                eventType: Constants.EVENT.PokemonSelected,
+                id: this.props.id
+            });
+        }
+    }
+
     render() { 
+
         let pokemon = PokedexHelper.getPokemon(this.props.id);
         let name = PokedexHelper.loc(pokemon);
-        var gen = pokemon.gen;
-        var h2Class = ''; //(args.inactive) ? 'inactive-h2' : '';
-        var valueClass = 'value'; //(args.inactive) ? 'inactive-value' : 'value';
-
-        var speciesHtml = '';
-        for (var i = 0; i < pokemon.species.length; i++) {
-            speciesHtml += pokemon.species[i] + ' '; //
-        }
+        let h2Class = this.props.inactive ? 'inactive-h2' : '';
+        let valueClass = this.props.inactive ? 'inactive-value' : 'value';
 
         return (
-            <div className="gen" data-id={this.props.id}>
-                <a className="pokemon g1" href="#" data-gen={gen}>
-                <h2 className={h2Class}>{name}</h2>
-                    <div className="types">{speciesHtml}</div>
-                    <img className="lazimage" src={"https://www.serebii.net/art/th/" + this.props.id + ".png"} style={{ display: 'block' }}/>
-                    <div className="bar">
-                        <div className={valueClass}></div>
+            <div className="gen" data-id={ this.props.id } onClick={ (e) => this.onPokemonClicked(e) }>
+                <a className="pokemon g1" href="#" data-gen={ pokemon.gen }>
+                <h2 className={ h2Class }>{ name }</h2>
+                    <div className="types">
+                        { pokemon.species.map((id) => this.buildSpecies(id)) }
                     </div>
-                    <div className="id">{this.props.id}</div>
+                    <Lazimage className="lazimage" src='../assets/images/wait.gif' target={ "https://www.serebii.net/art/th/" + this.props.id + ".png" } style={{ display: 'block' }}/>
+                    <div className="bar">
+                        <div className={ valueClass }></div>
+                    </div>
+                    <div className="id">{ this.props.id }</div>
                 </a>
             </div>
         )
     }
 }
 
-
-
-/*
-Engine.prototype.toHtml = function (pokemonId, args) {
-
-
-    var speciesHtml = '';
-    for (var i = 0; i < pokemon.species.length; i++) {
-        speciesHtml += this.toSpeciesHtml(pokemon.species[i]);
-    }
-
-    var valueClass = (args.inactive) ? 'inactive-value' : 'value';
-    var h2Class = (args.inactive) ? 'inactive-h2' : '';
-
-    return '<div class="gen" data-id="' + pokemonId + '">' +
-                '<a class="pokemon g1" href="#" data-gen="' + gen + '">' +
-                '<h2 class="' + h2Class + '">' + name + '</h2>' +
-                    '<div class="types">' +
-                        speciesHtml +
-                    '</div>' +
-                    //'<img class="lazy" src="https://pokemon.gameinfo.io/images/thumbs/140/' + pokemonId + '.png" style="display: block;"/>' +
-
-                        '<img class="lazimage" src="img/wait.gif" data-src="https://www.serebii.net/art/th/' + pokemonId + '.png" style="display: block;"/>' +
-                    '<div class="bar">' +
-                        '<div class="' + valueClass + '"></div>' +
-                    '</div>' +
-                    '<div class="id">' + pokemonId + '</div>' +
-                '</a>' +
-            '</div>';
-};
-*/
 export default Pokemon
