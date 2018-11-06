52757f9e1203dab32e03c360806febed9091d963
i051684 <gilles.salinas@sap.com>
2018-11-06 10:47:13 +0100

    New shinies

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import PokedexHelper from './data/PokedexHelper'
 import '../assets/styles/styles.css'
 import NotFound from './components/NotFound';
 
-fetch(window.location.origin + '/config.json')    
+fetch(window.location.origin + '/config.json?_=' + Date.now())    
     .then(response => {
         if (!response.ok) {
             throw Error(response.status + '. Lors de la recuperation du fichier de configuration de Pokeman.')
